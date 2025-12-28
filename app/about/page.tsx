b26b7d5348c203e1ea786f4ec0e12f2a8637b164
seifRashid <rseif1218@gmail.com>
2025-12-28 23:48:59 +0300

    Add about page and animated background gradient hero section

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { BackgroundGradientAnimationDemo } from '../components/GradientBackground'
+
+function AboutPage() {
+  return (
+    <div>
+        <BackgroundGradientAnimationDemo />
+    </div>
+  )
+}
+
+export default AboutPage
--- app/components/NavBarDemo.tsx
+++ app/components/NavBarDemo.tsx
@@ -5,8 +5,8 @@ import { NavBar } from "@/components/ui/tubelight-navbar"
 
 export function NavBarDemo() {
   const navItems = [
-    { name: 'Home', url: '#', icon: Home },
-    { name: 'About', url: '#', icon: User },
+    { name: 'Home', url: '/', icon: Home },
+    { name: 'About', url: '/about', icon: User },
     { name: 'Projects', url: '#', icon: Briefcase },
     { name: 'Resume', url: '#', icon: FileText }
   ]

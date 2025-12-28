95520a02fbea525d3725db33e7817c6c11054472
seifRashid <rseif1218@gmail.com>
2025-12-28 23:36:04 +0300

    Add navigation bar with the NavBarDemo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { NavBarDemo } from "./components/NavBarDemo";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <NavBarDemo />
         {children}
       </body>
     </html>
